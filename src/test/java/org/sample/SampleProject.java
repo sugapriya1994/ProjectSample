6c815c7cc4dc64c8c2e29d0e2ca79e95b61800dd
sugpriya1994 <sugapriyaece94@gmail.com>
2022-09-06 15:02:32 +0400

    Suga code

--- src/test/java/org/sample/SampleProject.java
+++ src/test/java/org/sample/SampleProject.java
@@ -1,5 +1,10 @@
 package org.sample;
 
 public class SampleProject {
-
+public static void main(String[] args) {
+	System.out.println("Test 1");
+	System.out.println("Test 2");
+	System.out.println("Test 3");
+	System.out.println("Test 4");
+}
 }
