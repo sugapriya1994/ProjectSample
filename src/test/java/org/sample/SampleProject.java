dab8fed6e33926cbdf734ff9f6624d9106052668
sugpriya1994 <sugapriyaece94@gmail.com>
2022-09-06 15:19:39 +0400

    Maha code

--- src/test/java/org/sample/SampleProject.java
+++ src/test/java/org/sample/SampleProject.java
@@ -2,4 +2,12 @@ package org.sample;
 
 public class SampleProject {
 
+	public static void main(String[] args) {
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		System.out.println("Test 3");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
+	}
 }
